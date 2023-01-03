5e0898c5cf7d071eba9894d4d475c6bd302eadf4
Jayanth <jayanthvarma1501@gmail.com>
2023-01-03 16:18:53 +0530

    modifications

--- init_setup.sh
+++ init_setup.sh
@@ -1,9 +1,8 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Create conda environment"
-
+echo [$(date)]: "Creating conda env with python 3.8"
 conda create --prefix ./env python=3.8 -y
-echo [$(date)]: "Activate environment"
+echo [$(date)]: "activate env"
 source activate ./env
-echo [$(date)]: "Install dev requirements"
+echo [$(date)]: "intalling dev requirements"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
